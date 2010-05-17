4893afab602e30b2c4fb13ac5fa7256233a9ad78
Derek Neighbors <derek@sunderland.local>
2010-05-17 01:37:28 -0700

    who knows?

---
+++ spec/factories/user.rb
@@ -0,0 +1,15 @@
+Factory.define :user do |u|
+  u.last_request_at 'Mon May 17 01:36:21 -0700 2010'
+  u.created_at 'Mon May 17 01:36:21 -0700 2010'
+  u.crypted_password 'foo'
+  u.updated_at 'Mon May 17 01:36:21 -0700 2010'
+  u.current_login_ip 'foo'
+  u.current_login_at 'Mon May 17 01:36:21 -0700 2010'
+  u.password_salt 'foo'
+  u.login_count 1
+  u.persistence_token 'foo'
+  u.last_login_ip 'foo'
+  u.last_login_at 'Mon May 17 01:36:21 -0700 2010'
+  u.login 'foo'
+  u.email 'foo'
+end
