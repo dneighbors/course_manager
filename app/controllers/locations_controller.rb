30a7f5f3d14a0e25105d3b2fd023882b81f7c8cf
Derek Neighbors <derek@sunderland.local>
2010-07-21 19:50:59 -0700

    add missing files

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,14 @@
+class LocationsController < ApplicationController
+
+  # GET /locations/1
+  # GET /locations/1.xml
+  def show
+    @location = Location.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @location }
+    end
+  end
+
+end
