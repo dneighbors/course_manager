ca59dac2dec629a125cc8f8da7af4d38e91e48ed
Derek Neighbors <derek@sunderland.local>
2010-07-22 17:13:36 -0700

    update front page styling

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,6 +2,7 @@ class ContentsController < ApplicationController
   # GET /contents
   # GET /contents.xml
   def index
+    @courses = Course.all
     respond_to do |format|
       format.html 
     end
